f2af850d318b5ecf40b6fad5e1b6ad6e86c0954f
Felipe Xavier Braz <103685054+felipexavier26@users.noreply.github.com>
2025-01-08 12:19:05 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MusicaController;
 
 Route::get('/musicas', [MusicaController::class, 'index']);
-Route::get('/musicas/{musica}', [MusicaController::class, 'show']);
+Route::get('/musicas/{id}', [MusicaController::class, 'show']);
 Route::post('/musicas', [MusicaController::class, 'store']);
 Route::put('/musicas/{id}', [MusicaController::class, 'update']);
 Route::delete('/musicas/{id}', [MusicaController::class, 'destroy']);
